3e06e6cc308f3ed0ea128bba4c356386f8b173c0
Melisa Manios <melisa.manios@hippo.build>
2022-11-29 15:07:16 -0500

    Feat:Create Constructor

--- app.js
+++ app.js
@@ -1,8 +1,102 @@
 
+    const getDinos = () => [
+        {
+        species: 'Albertosaurus',
+        weight: 10505,
+        height: 26,
+        diet: 'carnivor',
+        where: 'North America',
+        when: 'Late Cretaceous',
+        fact: 'Albertosaurus was named by Henry Fairfield Osborn in a one-page note at the end of his 1905 description of Tyrannosaurus rex.',
+        image: 'Albertosaurus.jpg',
+        },
+        {
+        species: 'Triceratops',
+        weight: 13000,
+        height: 114,
+        diet: 'herbavor',
+        where: 'North America',
+        when: 'Late Cretaceous',
+        fact: 'First discovered in 1889 by Othniel Charles Marsh',
+        image: 'triceratops.png',
+        },
+        {
+        species: 'Tyrannosaurus Rex',
+        weight: 11905,
+        height: 144,
+        diet: 'carnivor',
+        where: 'North America',
+        when: 'Late Cretaceous',
+        fact: 'The largest known skull measures in at 5 feet long.',
+        image: 'tyrannosaurus rex.png',
+        },
+        {
+        species: 'Anklyosaurus',
+        weight: 10500,
+        height: 55,
+        diet: 'herbavor',
+        where: 'North America',
+        when: 'Late Cretaceous',
+        fact: 'Anklyosaurus survived for approximately 135 million years.',
+        image: 'anklyosaurus.png',
+        },
+        {
+        species: 'Brachiosaurus',
+        weight: 70000,
+        height: '372',
+        diet: 'herbavor',
+        where: 'North America',
+        when: 'Late Jurasic',
+        fact: 'An asteroid was named 9954 Brachiosaurus in 1991.',
+        image: 'brachiosaurus.png',
+        },
+        {
+        species: 'Stegosaurus',
+        weight: 11600,
+        height: 79,
+        diet: 'herbavor',
+        where: 'North America, Europe, Asia',
+        when: 'Late Jurasic to Early Cretaceous',
+        fact: 'The Stegosaurus had between 17 and 22 seperate places and flat spines.',
+        image: 'stegosaurus.png',
+        },
+        {
+        species: 'Elasmosaurus',
+        weight: 16000,
+        height: 59,
+        diet: 'carnivor',
+        where: 'North America',
+        when: 'Late Cretaceous',
+        fact: 'Elasmosaurus was a marine reptile first discovered in Kansas.',
+        image: 'elasmosaurus.png',
+        },
+        {
+        species: 'Pigeon',
+        weight: 0.5,
+        height: 9,
+        diet: 'herbavor',
+        where: 'World Wide',
+        when: 'Holocene',
+        fact: 'All birds are living dinosaurs.',
+        image: 'pigeon.png',
+        },
+    ];
+    
+    
     // Create Dino Constructor
+    class Dino {
+        constructor({ species, weight, height, diet, where, when, fact, image }) {
+        this.image = image
+        this.species = species
+        this.weight = weight
+        this.height = height
+        this.diet = diet
+        this.where = where
+        this.when = when
+        this.fact = fact
+        }
 
 
-    // Create Dino Objects
 
 
     // Create Human Object
