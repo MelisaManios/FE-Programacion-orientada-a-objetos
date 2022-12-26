4f03935232cddca957cbf21686e456c4b1871e25
Melisa Manios <melisa.manios@hippo.build>
2022-12-26 15:33:44 -0500

    feat:  comparacion metricas

--- app.js
+++ app.js
@@ -1,189 +1,232 @@
+const getDinos = () => [
+  {
+    species: "Albertosaurus",
+    weight: 10505,
+    height: 26,
+    diet: "carnivor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "Albertosaurus was named by Henry Fairfield Osborn in a one-page note at the end of his 1905 description of Tyrannosaurus rex.",
+    image: "Albertosaurus.jpg",
+  },
+  {
+    species: "Triceratops",
+    weight: 13000,
+    height: 114,
+    diet: "herbavor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "First discovered in 1889 by Othniel Charles Marsh",
+    image: "triceratops.png",
+  },
+  {
+    species: "Tyrannosaurus Rex",
+    weight: 11905,
+    height: 144,
+    diet: "carnivor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "The largest known skull measures in at 5 feet long.",
+    image: "tyrannosaurus rex.png",
+  },
+  {
+    species: "Anklyosaurus",
+    weight: 10500,
+    height: 55,
+    diet: "herbavor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "Anklyosaurus survived for approximately 135 million years.",
+    image: "anklyosaurus.png",
+  },
+  {
+    species: "Brachiosaurus",
+    weight: 70000,
+    height: "372",
+    diet: "herbavor",
+    where: "North America",
+    when: "Late Jurasic",
+    fact: "An asteroid was named 9954 Brachiosaurus in 1991.",
+    image: "brachiosaurus.png",
+  },
+  {
+    species: "Stegosaurus",
+    weight: 11600,
+    height: 79,
+    diet: "herbavor",
+    where: "North America, Europe, Asia",
+    when: "Late Jurasic to Early Cretaceous",
+    fact: "The Stegosaurus had between 17 and 22 seperate places and flat spines.",
+    image: "stegosaurus.png",
+  },
+  {
+    species: "Elasmosaurus",
+    weight: 16000,
+    height: 59,
+    diet: "carnivor",
+    where: "North America",
+    when: "Late Cretaceous",
+    fact: "Elasmosaurus was a marine reptile first discovered in Kansas.",
+    image: "elasmosaurus.png",
+  },
+  {
+    species: "Pigeon",
+    weight: 0.5,
+    height: 9,
+    diet: "herbavor",
+    where: "World Wide",
+    when: "Holocene",
+    fact: "All birds are living dinosaurs.",
+    image: "pigeon.png",
+  },
+];
 
-    const getDinos = () => [
-        {
-        species: 'Albertosaurus',
-        weight: 10505,
-        height: 26,
-        diet: 'carnivor',
-        where: 'North America',
-        when: 'Late Cretaceous',
-        fact: 'Albertosaurus was named by Henry Fairfield Osborn in a one-page note at the end of his 1905 description of Tyrannosaurus rex.',
-        image: 'Albertosaurus.jpg',
-        },
-        {
-        species: 'Triceratops',
-        weight: 13000,
-        height: 114,
-        diet: 'herbavor',
-        where: 'North America',
-        when: 'Late Cretaceous',
-        fact: 'First discovered in 1889 by Othniel Charles Marsh',
-        image: 'triceratops.png',
-        },
-        {
-        species: 'Tyrannosaurus Rex',
-        weight: 11905,
-        height: 144,
-        diet: 'carnivor',
-        where: 'North America',
-        when: 'Late Cretaceous',
-        fact: 'The largest known skull measures in at 5 feet long.',
-        image: 'tyrannosaurus rex.png',
-        },
-        {
-        species: 'Anklyosaurus',
-        weight: 10500,
-        height: 55,
-        diet: 'herbavor',
-        where: 'North America',
-        when: 'Late Cretaceous',
-        fact: 'Anklyosaurus survived for approximately 135 million years.',
-        image: 'anklyosaurus.png',
-        },
-        {
-        species: 'Brachiosaurus',
-        weight: 70000,
-        height: '372',
-        diet: 'herbavor',
-        where: 'North America',
-        when: 'Late Jurasic',
-        fact: 'An asteroid was named 9954 Brachiosaurus in 1991.',
-        image: 'brachiosaurus.png',
-        },
-        {
-        species: 'Stegosaurus',
-        weight: 11600,
-        height: 79,
-        diet: 'herbavor',
-        where: 'North America, Europe, Asia',
-        when: 'Late Jurasic to Early Cretaceous',
-        fact: 'The Stegosaurus had between 17 and 22 seperate places and flat spines.',
-        image: 'stegosaurus.png',
-        },
-        {
-        species: 'Elasmosaurus',
-        weight: 16000,
-        height: 59,
-        diet: 'carnivor',
-        where: 'North America',
-        when: 'Late Cretaceous',
-        fact: 'Elasmosaurus was a marine reptile first discovered in Kansas.',
-        image: 'elasmosaurus.png',
-        },
-        {
-        species: 'Pigeon',
-        weight: 0.5,
-        height: 9,
-        diet: 'herbavor',
-        where: 'World Wide',
-        when: 'Holocene',
-        fact: 'All birds are living dinosaurs.',
-        image: 'pigeon.png',
-        },
-    ];
-    
-    
-    // Create Dino Constructor
-    class Dino {
-        constructor({ species, weight, height, diet, where, when, fact, image }) {
-        this.image = image
-        this.species = species
-        this.weight = weight
-        this.height = height
-        this.diet = diet
-        this.where = where
-        this.when = when
-        this.fact = fact
-        }
+class Dino {
+  constructor({ species, weight, height, diet, where, when, fact, image }) {
+    this.image = image;
+    this.species = species;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+    this.where = where;
+    this.when = when;
+    this.fact = fact;
+  }
 
-        getHecho(human) {
-            const hecho = this.species === 'Pigeon' ? 0 : Math.round(Math.random() * 5);
-            if (hecho == 1) return this.getWeight(human.weight)
-            if (hecho == 2) return this.getHeight(human.height)
-            if (hecho == 3) return this.getDiet(human.diet)
-            if (hecho == 4) return `The dino was from ${this.where}`
-            if (hecho == 5) return `The dino was in ${this.when}`
-            return this.fact
-        }
-        
-        getWeight(weightHuman) {
-            if (weightHuman == this.weight) return `The weight of the dino is equal to human`
-            return (weightHuman > this.weight)
-            ? `The weight of the dino is smaller than the human`
-            : `The weight of the dino is greater than the human`
-        }
-        
-        getHeight(heightHuman) {
-            if (heightHuman == this.height) return `The height of the dino is equal to human`
-            return (heightHuman > this.height)
-            ? `The height of the dino is smaller than the human`
-            : `The height of the dino is greater than the human`
-        }
-        
-        getDiet(dietHuman) {
-            return dietHuman == this.diet 
-            ? `The diet of the dino is equal to human`
-            : `The diet of the dino is different to human`
-        }
-        }
-        
-        const human = {
-        name: 'melisa',
-        height: 98,
-        weight: 78,
-        diet: 'carnivor'
-        }
-        
-        const getHtmlHuman = (name) => (`
+  getHecho(human) {
+    const hecho = this.species === "Pigeon" ? 0 : Math.round(Math.random() * 5);
+    if (hecho == 1) return this.getWeight(human.weight);
+    if (hecho == 2) return this.getHeight(human.height);
+    if (hecho == 3) return this.getDiet(human.diet);
+    if (hecho == 4) return `The dino was from ${this.where}`;
+    if (hecho == 5) return `The dino was in ${this.when}`;
+    return this.fact;
+  }
+
+  getWeight(weightHuman) {
+    if (weightHuman == this.weight)
+      return `The weight of the dino is equal to human`;
+    return weightHuman > this.weight
+      ? `The weight of the dino is smaller than the human`
+      : `The weight of the dino is greater than the human`;
+  }
+
+  getHeight(heightHuman) {
+    if (heightHuman == this.height)
+      return `The height of the dino is equal to human`;
+    return heightHuman > this.height
+      ? `The height of the dino is smaller than the human`
+      : `The height of the dino is greater than the human`;
+  }
+
+  getDiet(dietHuman) {
+    return dietHuman == this.diet
+      ? `The diet of the dino is equal to human`
+      : `The diet of the dino is different to human`;
+  }
+}
+
+
+
+const getHtmlHuman = (name) => `
         <div class="grid-item">
             <h3>${name}</h3>
             <img src="images/human.png">
             <p></p>
         </div>
-        `)
-        
-        const getHtmlDino = (dino, human) => (`
+        `;
+
+  
+
+const getHtmlDino = (dino, human) => `
         <div class="grid-item">
             <h3>${dino.species}</h3>
             <img src="images/${dino.image}">
             <p>${dino.getHecho(human)}</p>
         </div>
-        `)
-
-        const dinos = getDinos().map(dino => getHtmlDino(new Dino(dino), human))
-        dinos.splice(4, 0, getHtmlHuman(human.name))
-        
-        // document.getElementById('grid').appendChild(fragment);
-        document.querySelector('#grid').innerHTML = dinos
+        `;
 
 
-    function obtenerDatos(){
-        let name =document.getElementById('name').value;
+const compare = (human) => {
+  document.getElementById('dino-compare').style.display = 'none'
+  const dinos = getDinos().map((dino) => getHtmlDino(new Dino(dino), human));
+  dinos.splice(4, 0, getHtmlHuman(human.name));
+  document.querySelector("#grid").innerHTML = dinos;
+  document.getElementById('grid-dinos').style.display = 'block'
+}
+
+const obtenerDatos = () => {
+  let height, weight;
+  if (document.getElementById('metric').checked) {
+    height = Math.round(parseFloat(document.getElementById('height-metric').value) / 2.21)
+    weight = Math.round(parseFloat(document.getElementById('weight-metric').value) * 2.54)
+  } else {
+    height = (document.getElementById('feet').value * 12) + Number(document.getElementById('inches').value)
+    weight = document.getElementById('weight').value
+  }
+  return {
+    name: document.getElementById('name').value,
+    diet: document.getElementById('diet').value,
+    height,
+    weight,
+  }
+}
+
+const resetForm = () => {
+  document.getElementById('name').value = ''
+  document.getElementById('diet').value = 'Herbavor'
+  document.getElementById('imperial-form').style.display = 'none'
+  document.getElementById('height-metric').value = ''
+  document.getElementById('weight-metric').value = ''
+  document.getElementById('inches').value = ''
+  document.getElementById('feet').value = ''
+  document.getElementById('weight').value = ''
+  document.getElementById('metric').checked = true
+}
+
+const compareButton = document.getElementById('btn-compare')
+const repeatButton = document.getElementById('repeat-btn')
+
+compareButton.addEventListener('click', () => {
+  if (document.getElementById('name').value == '') return alert('required name')
+  if (document.getElementById('diet').value == '') return alert('required diet')
+  if (document.getElementById('metric').checked) {
+    if (!(
+      document.getElementById('height-metric').value && document.getElementById('weight-metric').value 
+    )) {
+      return alert('required height, weight')
     }
+  } else {
+    if (!(
+      document.getElementById('inches').value &&
+      document.getElementById('feet').value &&
+      document.getElementById('weight').value
+    )) {
+      return alert('required inches, feet, weight')
+    }
+  }
+  const human = obtenerDatos()
+  compare(human)
+})
+
+repeatButton.addEventListener('click', () => {
+  document.getElementById('grid-dinos').style.display = 'none'
+  document.getElementById('dino-compare').style.display = 'block'
+  resetForm()
+})
 
 
-    // Create Human Object
-
-    // Use IIFE to get human data from form
-
-
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
-
-
-// On button click, prepare and display infographic
+const changeUnits = (id) => {
+  if (id == 'metric') {
+    document.getElementById('inches').value = ''
+    document.getElementById('feet').value = ''
+    document.getElementById('weight').value = ''
+    document.getElementById('imperial-form').style.display = 'none'
+    document.getElementById('metric-form').style.display = 'block'
+  } else {
+    document.getElementById('height-metric').value = ''
+    document.getElementById('weight-metric').value = ''
+    document.getElementById('metric-form').style.display = 'none'
+    document.getElementById('imperial-form').style.display = 'block'
+  }
+}
